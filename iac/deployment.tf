0dbfb70605d6964e9a761fe6d11a9e99602fb5a8
Angel Perez <angyay0@gmail.com>
2021-01-26 18:26:04 -0600

    Updates for stable Iac

--- iac/deployment.tf
+++ iac/deployment.tf
@@ -33,6 +33,7 @@ resource "aws_subnet" "publicsnet" {
   vpc_id     = aws_vpc.chall_vpc.id
   cidr_block = "172.16.43.0/28"
   map_public_ip_on_launch = true
+  availability_zone = "us-east-1b"
 
   tags = {
     Environment = "Challenge"
@@ -41,7 +42,8 @@ resource "aws_subnet" "publicsnet" {
 resource "aws_subnet" "privatesnet" {
   vpc_id     = aws_vpc.chall_vpc.id
   cidr_block = "172.16.43.16/28"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
+  availability_zone = "us-east-1b"
 
   tags = {
     Environment = "Challenge"
@@ -55,23 +57,7 @@ resource "aws_internet_gateway" "chall_igw" {
     Environment = "Challenge"
   }
 }
-#Routing Public Subnet
-resource "aws_route_table" "chall_route_public" {
-  vpc_id = aws_vpc.chall_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.chall_igw.id
-  }
-
-  tags = {
-    Environment = "Challenge"
-  }
-}
-resource "aws_route_table_association" "chall_route_assoc_public" {
-  subnet_id = aws_subnet.publicsnet.id
-  route_table_id = aws_route_table.chall_route_public.id
-}
+#EIP
 resource "aws_eip" "publiceip" {
   vpc = true
 }
@@ -84,26 +70,147 @@ resource "aws_nat_gateway" "chall_nat" {
     Environment = "Challenge"
   }
 }
+#Routing Public Subnet
+resource "aws_route_table" "chall_route_public" {
+  vpc_id = aws_vpc.chall_vpc.id
+
+  tags = {
+    Environment = "Challenge"
+  }
+}
+resource "aws_route" "chall_pub_route" {
+  route_table_id = aws_route_table.chall_route_public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.chall_igw.id
+}
+resource "aws_route_table_association" "chall_route_assoc_public" {
+  subnet_id = aws_subnet.publicsnet.id
+  route_table_id = aws_route_table.chall_route_public.id
+}
+#Routing Private Subnet
 resource "aws_route_table" "chall_route_nat" {
   vpc_id = aws_vpc.chall_vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.chall_nat.id
+  tags = {
+    Environment = "Challenge"
+  }
+}
+resource "aws_route" "chall_priv_route" {
+  route_table_id = aws_route_table.chall_route_nat.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.chall_nat.id
+}
+resource "aws_route_table_association" "chall_route_assoc_nat" {
+  subnet_id = aws_subnet.privatesnet.id
+  route_table_id = aws_route_table.chall_route_nat.id
+}
+#Security Groups
+resource "aws_security_group" "chall_sg_nat" {
+  name        = "chall_nat"
+  description = "Traffic Control"
+  vpc_id      = aws_vpc.chall_vpc.id
+  #Incoming
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_subnet.privatesnet.cidr_block}"]
+  }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_subnet.privatesnet.cidr_block}"]
+  }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  #Egress
+  egress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_vpc.chall_vpc.cidr_block}"]
+  }
+  egress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
     Environment = "Challenge"
   }
 }
-resource "aws_route_table_association" "chall_route_assoc_nat" {
-  subnet_id = aws_subnet.privatesnet.id
-  route_table_id = aws_route_table.chall_route_nat.id
-}
+resource "aws_security_group" "chall_sg_pub" {
+  name        = "challsgpub"
+  description = "Traffic Control"
+  vpc_id      = aws_vpc.chall_vpc.id
+  #Incoming
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "ICMP"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  #Egress
+  egress {
+    description = "For Internal"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#Security Group 
-resource "aws_security_group" "chall_sg" {
-  name        = "chall_nat"
+  tags = {
+    Environment = "Challenge"
+  }
+}
+resource "aws_security_group" "chall_sg_priv" {
+  name        = "challsgpriv"
   description = "Traffic Control"
   vpc_id      = aws_vpc.chall_vpc.id
   #Incoming
@@ -126,21 +233,29 @@ resource "aws_security_group" "chall_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${aws_vpc.chall_vpc.cidr_block}"]
   }
   ingress {
     description = "ICMP"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "ICMP"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["${aws_vpc.chall_vpc.cidr_block}"]
+  }
+  #Egress
+  egress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   egress {
-    description = "Output"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -151,23 +266,69 @@ resource "aws_security_group" "chall_sg" {
 ###############################
 ##   AWS App Components      ##
 ###############################
-#KeyPair
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
+#Jumpbox
+resource "aws_instance" "jumpbox" {
+    ami = "ami-01ef31f9f39c5aaed"
+    instance_type = "t2.micro"
+    key_name = "DevOpsKey"
+    vpc_security_group_ids = ["${aws_security_group.chall_sg_nat.id}"]
+    subnet_id = "${aws_subnet.publicsnet.id}"
+    associate_public_ip_address = true
+    source_dest_check = false
+
+    tags = {
+        Name = "VPC NAT"
+        Environment = "Challenge"
+    }
+
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    host = self.public_ip
+    private_key = file("DevOpsKey.pem")
+  }
+
+  provisioner "file" {
+    source = "DevOpsKey.pem"
+    destination = "/home/ec2-user/DevOpsKey.pem"
+  }
+
+  provisioner "file" {
+    source = "showip.conf"
+    destination = "/home/ec2-user/showip"
+  }
 }
-#EC2 Instance
+resource "aws_eip" "jumboxeip" {
+    instance = "${aws_instance.jumpbox.id}"
+    vpc = true
+}
+
+#EC2 Server Instance
 resource "aws_instance" "chall_instance" {
   ami = "ami-00ddb0e5626798373"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.privatesnet.id
-  key_name = "${aws_key_pair.deployer.id}"
-  vpc_security_group_ids = [ "${aws_security_group.chall_sg.id}" ]
+  associate_public_ip_address = false
+  key_name = "DevOpsKey"
+  vpc_security_group_ids = [ "${aws_security_group.chall_sg_priv.id}" ]
 
   tags = {
     Environment = "Challenge"
   }
-  
+
+  #provisioner "remote-exec" {
+  #   inline = [
+  #    "chmod 400 DevOpsKey.pem",
+  #    "ssh -i \"DevOpsKey.pem\" ubuntu@${self.private_ip}",
+  #    "sudo apt-get -y update",
+  #    "sudo apt-get -y install nginx",
+  #    "sudo service nginx start",
+  #    "sudo cp showip /etc/nginx/sites-available/showip",
+  #    "sudo rm /etc/nginx/sites-enabled/default",
+  #    "sudo service nginx restart"
+  #  ]
+  #}
+
 }
 ###############################
 ##   AWS App Components      ##
@@ -175,7 +336,7 @@ resource "aws_instance" "chall_instance" {
 #ELB
 resource "aws_elb" "chall-elb" {
   name = "chall-elb"
-  security_groups = [ "${aws_security_group.chall_sg.id}" ]
+  security_groups = [ "${aws_security_group.chall_sg_pub.id}" ]
   subnets = [ "${aws_subnet.publicsnet.id}"]
   instances = [ "${aws_instance.chall_instance.id}" ]
 
@@ -186,10 +347,6 @@ resource "aws_elb" "chall-elb" {
     lb_protocol = "http"
   }
 }
-###############################
-##   Bonus                   ##
-###############################
-#TODO
 ###############################
 ##   Output                  ##
 ###############################
---
+++ readme.md
@@ -0,0 +1,62 @@
+# Solution
+El archivo de despliegue contiene las configuraciones para el despliegue a través de pipelines para la siguiente arquitectura
+
+![Arquitectura](imgs/solarch.jpg)
+
+## Red
+La configuración de red va sobre un conjunto limitado de ips en el VPC, ya que para optimizar los recursos, es necesario limitar para no tener direcciones de sobra, esto se tiene que ajustar cuando se agreguen reglas de autoescalado.
+
+- Segmento /26 => 64  Direcciones IP
+- Subred Publica /28 => 16 Direcciones
+- Subres Privada /28 => 16 Direcciones
+
+El enrutamiento incluye una Nat, Internet  Gateway y un conjunto de reglas para cada ambiente
+
+- SG-NAT: habilita puertos 22, 80, 443, habilitando la funcion para saltos.
+- SG-PUB: habilita puertos 80 y 443.
+- SG-PRIV: habilita puertos 22 (Solo Jumpbox), 80 y 443 (Restringido para VPC)
+
+
+## Administración de Instancia(s)
+Para la administración, decidí utilizar un Jumpbox, ya que de esa forma no exponemos los puertos a nivel público, ya que se necesita de un servidor especial para hacer esta funcion.
+
+Se uso una imagen especifica para la region EAST configurada para operaciones de Jumpbox (*Bastion*).
+
+Se accede a través de la red publica al servidor y este funciona de jump para la o las instancias en la red privada.
+
+La jumpbox recibe del proceso la inyección de la llave de administración y el archivo de contexto de nginx.
+
+
+## Proceso
+El proceso se encuentra semi autimatizado, la parte que se ejecutó de manera manual es el DSC o la preparación el EC2 para regresar la ip.
+
+Los comandos que se encuentran en la sección DSC Script es lo que se ejecutó manualmente del proceso.
+
+## Runtime
+La ejecución se ejecutó a través de Azure DevOps/Local usando la extension Token Replacement, para evitar colocar datos sensibles de AWS en los archivos, la validación se ejecutó en entorno local y en la nube.
+
+
+Se incluyen los logs de ejecución a travéz de Azure Pipelines en */logs*
+
+## IAM, Key Pair
+Se uso una entidad para _IAM_ y poder utilizarla en el proceso de despliegue, tiene las siguientes reglas habilitadas:
+- VPCFullAccess
+- EC2FullAccess
+- S3FullAccess
+- ElasticLoadBalancingFullAccess
+
+Para _Key Pair_ se generó uno con el servicio de AWS y se descargo para el despliegue de las intancias y este mismo archivo es cargado al Jumpbox automáticamente. (Por seguridad no se incluyen aquí)
+
+De esta manera tenemos control de que el rol unicamente hace o tiene acceso a lo que se supone para el proceso de despliegue y conexión. 
+
+### DSC Script
+```
+    sudo apt-get update -y
+    sudo apt-get install nginx
+    sudo service nginx start
+    sudo nano /etc/nginx/sites-available/showip
+        _El contenido se encuentra en iac/showip.conf_
+    sudo ln -s /etc/nginx/sites-available/showip /etc/nginx/sites-enabled/showip
+    sudo rm /etc/nginx/sites-enabled/default
+    sudo service nginx restart
+```
